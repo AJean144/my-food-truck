c1a2865d11abcbc1cafb523ada26f336b65ba901
AJean144 <ajeanjacques42@gmail.com>
2018-08-29 14:29:28 -0400

    First commit

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyAXMP6VcBAmN7FyzAWAQ7YFISq0b2bOfhg";
+export const FIREBASE_AUTH_DOMAIN = "my-food-truck.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://my-food-truck.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "my-food-truck";
+export const FIREBASE_STORAGE_BUCKET = "my-food-truck.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "748690316495";
+
+export const FACEBOOK_APP_ID = "1643569469065876";
